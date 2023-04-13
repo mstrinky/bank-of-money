51cab28ee572fdd65db392e1b113f7d7293828e7
Mst Rinky Akter <mstrinkya@gmail.com>
2023-04-13 23:44:55 +0600

    pending solution

--- bank.js
+++ bank.js
@@ -26,3 +26,20 @@ document.getElementById('calculate').addEventListener('click',function(){
     balanceField.innerText = balance;
 
 })
+
+
+
+document.getElementById('save').addEventListener('click',function(){
+
+    const balanceField = document.getElementById('balance');
+    const balance = income - totalExpense ;
+    balanceField.innerText = balance;
+
+    const amountOfSave = document.getElementById('amountof-save');
+    const amountString = amountOfSave.value;
+    const amount = parseInt(amountString);
+
+   const savingAmount = balanceField - amount/100;
+
+ 
+})
